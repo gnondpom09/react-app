0b54cd1b3e9fc372d374c6aff87396ac8bcaac4d
AC75097548 <laurent.botella@capgemini.com>
2021-03-11 11:06:06 +0100

    Add bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
